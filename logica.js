d4979f45cc9f1513d467e479ceb294bee8ed7665
Kaue Francisco <90930907+kaue-f@users.noreply.github.com>
2023-01-03 23:41:18 -0300

    Add files via upload

--- logica.js
+++ logica.js
@@ -1,43 +1,44 @@
 //variaveis 
-  var js_a;
-  var js_b;
-  var js_c;
-  var js_delta;
-  var js_x1;
-  var js_x2;
+  var a;
+  var b;
+  var c;
+  var delta;
+  var x1;
+  var x2;
             
-//função calcular
+//função de calcular a formula de bhaskara
   function calcular(){       
-//entrada dados
-  var js_a = parseFloat(document.getElementById("a").value);
-  var js_b = parseFloat(document.getElementById("b").value);
-  var js_c = parseFloat(document.getElementById("c").value);
 
-  if (!js_b || !js_c) {
+//entrada dados 
+  var a = parseFloat(document.getElementById("a").value);
+  var b = parseFloat(document.getElementById("b").value);
+  var c = parseFloat(document.getElementById("c").value);
+
+  //Inicio logico da formula de bhaskara
+  if (!b || !c) {
      alert("Insirar o Valor!!");
   } 
 
-  if (js_a != 0) {
-    js_delta = (js_b*js_b) - ((4*js_a)*js_c);
-    document.getElementById("delta").innerHTML = "Valor de Delta: " +  js_delta;
+  if (a != 0) {
+    delta = (b*b) - ((4*a)*c);
+    document.getElementById("delta").innerHTML = "Valor de Delta: " +  delta;
   
-  if (js_delta < 0){
-    document.getElementById("delta").innerHTML = "Valor de Delta: " + js_delta + ("<p>Delta não tem raiz real!!");
+  if (delta < 0){
+    document.getElementById("delta").innerHTML = "Valor de Delta: " + delta + ("<p>Delta não tem raiz real!!");
   }
-  if (js_delta == 0){
-    js_x1 = (-js_b )/(2*js_a);
-    js_x2 = (-js_b )/(2*js_a);
-    document.getElementById("x1").innerHTML = "Valor de x1: " + js_x1.toFixed(4);
-    document.getElementById("x2").innerHTML = "Valor de x2: " + js_x2.toFixed(4);
+  if (delta == 0){
+    x1 = (-b )/(2*a);
+    x2 = (-b )/(2*a);
+    document.getElementById("x1").innerHTML = "Valor de x1: " + x1.toFixed(4);
+    document.getElementById("x2").innerHTML = "Valor de x2: " + x2.toFixed(4);
   }
-  if (js_delta > 0){
-    js_x1 = (-js_b + Math.sqrt(js_delta))/(2*js_a);
-    js_x2 = (-js_b - Math.sqrt(js_delta))/(2*js_a);
-    document.getElementById("x1").innerHTML = "Valor de x1: " + js_x1.toFixed(4);
-    document.getElementById("x2").innerHTML = "Valor de x2: " + js_x2.toFixed(4);
+  if (delta > 0){
+    x1 = (- + Math.sqrt(delta))/(2*a);
+    x2 = (-b - Math.sqrt(delta))/(2*a);
+    document.getElementById("x1").innerHTML = "Valor de x1: " + x1.toFixed(4);
+    document.getElementById("x2").innerHTML = "Valor de x2: " + x2.toFixed(4);
   }
 } else{
   alert('O valor de "a" tem que ser diferente de 0!!');
 }
-
 }
